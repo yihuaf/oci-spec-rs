1b1f9d59b859531c6c78cbdd26ce2eac6c0aa186
utam0k <utam0k@users.noreply.github.com>
2021-08-24 12:24:02 +0000

    deploy: 31b548852b7b0c2e973fe445597fef26ffa185c5

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["oci_spec"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;'_ <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.54.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"oci_spec/runtime/enum.LinuxNamespaceType.html\" title=\"enum oci_spec::runtime::LinuxNamespaceType\">LinuxNamespaceType</a>","synthetic":false,"types":["oci_spec::runtime::linux::LinuxNamespaceType"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["env_logger"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"env_logger/fmt/struct.Formatter.html\" title=\"struct env_logger::fmt::Formatter\">Formatter</a>","synthetic":false,"types":["env_logger::fmt::Formatter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
